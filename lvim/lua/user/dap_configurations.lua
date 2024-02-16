3252d28ac771c850b2dbb766aff1e787b2c6bc84
Michael <mixkok2001@gmail.com>
2024-02-16 12:17:44 +0200

    DAP for cpp and typst config saved (not working yet)

--- lvim/lua/user/dap_configurations.lua
+++ lvim/lua/user/dap_configurations.lua
@@ -1,23 +1,5 @@
 local dap = require('dap')
 
--- dap.configuration.cpp = {
---   type = 'cpp',
---   request = 'launch',
--- }
-
---[[ dap.configuration.cpp = {
-  type = 'executable',
-  attach = {
-    pidProperty = "pid",
-    pidSelect = "ask"
-  },
-  command = 'lldb-vscode-11',  -- my binary was called 'lldb-vscode-11
-  env = {
-    LLDB_LAUNCH_FLAG_LAUNCH_IN_TTY = "YES"
-  },
-  name = "lldb"
-} ]]
-
 dap.configurations.cpp = {
     {
         name = "Launch file",
