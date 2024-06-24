544a9002b33b24d27744c978f56cbaeb23550976
Michael <mixkok2001@gmail.com>
2024-06-24 14:39:10 +0300

    Added keymaps for resizing buffers

--- nvim/lua/keymaps/buffer.lua
+++ nvim/lua/keymaps/buffer.lua
@@ -6,3 +6,9 @@ vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true 
 vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
 -- Map Ctrl+l to move to the right buffer
 vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+
+-- Resize buffers
+vim.api.nvim_set_keymap('n', '<C-Left>','<C-w><', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-Right>','<C-w>>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-Up>','<C-w>+', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-Down>','<C-w>-', { noremap = true, silent = true })
