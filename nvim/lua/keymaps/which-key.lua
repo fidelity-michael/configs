b1f84f8b45045c7c637000ff66e4c18d685c22d8
Michael <mixkok2001@gmail.com>
2024-06-27 00:36:40 +0300

    Added descriptio for file tree in which-key keymap

--- nvim/lua/keymaps/which-key.lua
+++ nvim/lua/keymaps/which-key.lua
@@ -7,4 +7,5 @@ require('which-key').register {
   ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
   ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
   ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+  ['<leader>e'] = { name = 'File tree', _ = 'which_key_ignore' },
 }
