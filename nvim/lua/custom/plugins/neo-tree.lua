0a6f30d4056c1629bc210b0d165ca1d2608dc1d0
Michael <mixkok2001@gmail.com>
2024-06-27 12:13:40 +0300

    Added telescope actions and removed unnecessary neo-tree declaration in lazy.lua

--- nvim/lua/custom/plugins/neo-tree.lua
+++ nvim/lua/custom/plugins/neo-tree.lua
@@ -1,31 +1,3 @@
--- Unless you are still migrating, remove the deprecated commands from v1.x
--- vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
---
--- return {
---   "nvim-neo-tree/neo-tree.nvim",
---   version = "*",
---   dependencies = {
---     "nvim-lua/plenary.nvim",
---     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
---     "MunifTanjim/nui.nvim",
---   },
---   config = function ()
---     require('neo-tree').setup {}
---   end,
--- }
-
--- return {
---   "nvim-tree/nvim-tree.lua",
---   version = "*",
---   lazy = false,
---   dependencies = {
---     "nvim-tree/nvim-web-devicons",
---   },
---   config = function()
---     require("nvim-tree").setup {}
---   end,
--- }
-
 return {
   "nvim-tree/nvim-tree.lua",
   dependencies = "nvim-tree/nvim-web-devicons",
--- nvim/lua/telescope/telescope.lua
+++ nvim/lua/telescope/telescope.lua
@@ -1,9 +1,14 @@
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
+local actions = require('telescope.actions')
 require('telescope').setup {
   defaults = {
+    path_display = {"smart"},
     mappings = {
       i = {
+        ['<C-k>'] = actions.move_selection_previous,
+        ['<C-j>'] = actions.move_selection_next,
+        ['<C-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
         ['<C-u>'] = false,
         ['<C-d>'] = false,
       },
