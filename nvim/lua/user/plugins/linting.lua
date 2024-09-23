02ca37249416da5e3cd77d4c1023d2b7cd684983
Michael <mixkok2001@gmail.com>
2024-09-23 14:24:36 +0300

    Added eslint support for typescript linting

--- nvim/lua/user/plugins/linting.lua
+++ nvim/lua/user/plugins/linting.lua
@@ -22,6 +22,18 @@ return {
       end,
     })
 
+    local eslint = lint.linters.eslint_d
+    eslint.args = {
+      '--no-warn-ignored',
+      '--format',
+      'json',
+      '--stdin',
+      '--stdin-filename',
+      function()
+        return vim.api.nvim_buf_get_name(0)
+      end,
+    }
+
     vim.keymap.set('n', '<leader>ll', function()
       lint.try_lint()
     end, { desc = 'Trigger linting for current file' })
