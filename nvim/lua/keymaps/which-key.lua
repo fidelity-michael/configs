c3f33e94527023903449afcb18f08f89725e88ec
Michael <mixkok2001@gmail.com>
2024-09-23 14:50:38 +0300

    Added auto-session plugin for saving/restoring sessions

--- nvim/lua/keymaps/which-key.lua
+++ nvim/lua/keymaps/which-key.lua
@@ -7,6 +7,7 @@ require('which-key').register {
   ['<leader>r'] = { name = 'Restart lsp server', _ = 'which_key_ignore' },
   ['<leader>s'] = { name = 'Search', _ = 'which_key_ignore' },
   ['<leader>w'] = { name = 'Save file', _ = 'which_key_ignore' },
+  ['<leader>a'] = { name = 'Save/Restore Sessions', _ = 'which_key_ignore' },
   ['<leader>e'] = { name = 'File tree', _ = 'which_key_ignore' },
   ['<leader>x'] = { name = 'Diagnostics', _ = 'which_key_ignore' },
   ['<leader>l'] = { name = 'Lsp', _ = 'which_key_ignore' },
