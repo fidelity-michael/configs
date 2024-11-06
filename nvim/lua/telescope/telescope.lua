85dcd1526f4141260469e0ee09e59b013571e4e0
Michael <mixkok2001@gmail.com>
2024-11-06 13:34:05 +0200

    Telescope can search for hidden files now (starting with ".")

--- nvim/lua/telescope/telescope.lua
+++ nvim/lua/telescope/telescope.lua
@@ -1,9 +1,10 @@
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
-local actions = require('telescope.actions')
+local actions = require 'telescope.actions'
 require('telescope').setup {
   defaults = {
-    path_display = {"smart"},
+    path_display = { 'smart' },
+    show_hidden = true,
     mappings = {
       i = {
         ['<C-k>'] = actions.move_selection_previous,
@@ -12,7 +13,7 @@ require('telescope').setup {
         ['<C-u>'] = false,
         ['<C-d>'] = false,
       },
-   },
+    },
   },
 }
 
@@ -27,17 +28,17 @@ local function find_git_root()
   local current_dir
   local cwd = vim.fn.getcwd()
   -- If the buffer is not associated with a file, return nil
-  if current_file == "" then
+  if current_file == '' then
     current_dir = cwd
   else
     -- Extract the directory from the current file's path
-    current_dir = vim.fn.fnamemodify(current_file, ":h")
+    current_dir = vim.fn.fnamemodify(current_file, ':h')
   end
 
   -- Find the Git root directory from the current file's path
-  local git_root = vim.fn.systemlist("git -C " .. vim.fn.escape(current_dir, " ") .. " rev-parse --show-toplevel")[1]
+  local git_root = vim.fn.systemlist('git -C ' .. vim.fn.escape(current_dir, ' ') .. ' rev-parse --show-toplevel')[1]
   if vim.v.shell_error ~= 0 then
-    print("Not a git repository. Searching on current working directory")
+    print 'Not a git repository. Searching on current working directory'
     return cwd
   end
   return git_root
@@ -47,9 +48,9 @@ end
 local function live_grep_git_root()
   local git_root = find_git_root()
   if git_root then
-    require('telescope.builtin').live_grep({
+    require('telescope.builtin').live_grep {
       search_dirs = { git_root },
-    })
+    }
   end
 end
 
