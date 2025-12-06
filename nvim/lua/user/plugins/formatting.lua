4f6489d32f2a29ab3c4ca82aa1b698c3affe6fc5
Michael <mixkok2001@gmail.com>
2025-12-06 14:54:03 +0200

    Added pretty-php formatter

--- nvim/lua/user/plugins/formatting.lua
+++ nvim/lua/user/plugins/formatting.lua
@@ -24,6 +24,7 @@ return {
           python = { 'isort', 'black' },
           cpp = { 'clang-format' },
           c = { 'clang-format' },
+          php = { 'pretty-php' },
         },
         -- format_on_save = {
         --   lsp_fallback = true,
